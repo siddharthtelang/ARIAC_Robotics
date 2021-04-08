84a624c229322a30855ccc2e6a58027c1ce3c1b6
tangm7 <mack.tang@colorado.edu>
2021-04-08 12:17:50 -0600

    just move implementations from rwa_implementation.h to the cpp file

--- rwa4_group_2/include/rwa_implementation.h
+++ rwa4_group_2/include/rwa_implementation.h
@@ -86,82 +86,10 @@ public:
     void checkAgvErrors();
 
 
-
-
-
-
-
-    double calcDistanceInXYPlane(geometry_msgs::Pose a, geometry_msgs::Pose b) {
-        return std::sqrt( std::pow(a.position.x - b.position.x, 2) + std::pow(a.position.y - b.position.y, 2) ); // euclidean distance
-    }
-
-
-
-
     struct distance_and_PresetLocation_struct {
         double distance;
         PresetLocation candidate_location;
     };
-
-    //////////////////////////////// Get nearest preset location to current gantry position
-    PresetLocation getNearesetPresetLocation() {
-        ////// Get current gantry position
-        geometry_msgs::Pose current_position = gantry_->getGantryPose();
-
-        ROS_INFO_STREAM("finished getGantry Pose successfully executed!");
-
-        ////// Define custom struct (distance, PresetLocation), see https://www.cplusplus.com/articles/NhA0RXSz/
-
-        ROS_INFO_STREAM("struct defined successfully");
-
-
-        ////// Build vector of custom structs
-        std::vector<distance_and_PresetLocation_struct> vector_of_structs;
-
-        ROS_INFO_STREAM("std::vector of structs ran");
-
-        ROS_INFO_STREAM("size of preset vector ==== " << preset_locations_list_.size());
-
-        ROS_INFO_STREAM("name of 1st of preset vector ==== " << preset_locations_list_.at(0).name);
-
-
-        for (PresetLocation pset_location : preset_locations_list_) {
-            ROS_INFO_STREAM("for loop entered");
-
-            geometry_msgs::Pose  location_converted_to_pose = gantryXY2worldposeXY(pset_location); // convert preset location to pose
-            ROS_INFO_STREAM("location_converted_to_pose in loop ran");
-
-            double distance_i = calcDistanceInXYPlane(current_position, location_converted_to_pose); // euclidean dist in xy plane
-            ROS_INFO_STREAM("calcDistanceInXYPlane in loop ran");
-
-            distance_and_PresetLocation_struct candidate_struct {distance_i, pset_location}; // make a struct
-            ROS_INFO_STREAM("candidate_struct in loop ran");
-
-            vector_of_structs.push_back(candidate_struct); // put struct in list of structs
-            ROS_INFO_STREAM("push_back in loop ran");
-
-        }
-
-        ROS_INFO_STREAM("vector of custom structs defined successfully");
-
-
-        ////// Sort vector of custom structs by distance (need *another* function to do this, will define a lambda function)
-        std::sort(vector_of_structs.begin(), vector_of_structs.end(),
-            // Lambda expression begins,
-            [](const distance_and_PresetLocation_struct &lhs, const distance_and_PresetLocation_struct &rhs) {
-                return (lhs.distance < rhs.distance);
-            } // end of lambda expression
-        );
-
-        ROS_INFO_STREAM("sorted vector successfully");
-
-
-        ////// Return the nearest PresetLocation
-        return vector_of_structs[0].candidate_location;
-    }
-
-    // std::unordered_map<std::string, double> asdfasdf;
-
 
     template <typename Container> // we can make this generic for any container [1]
     struct container_hash {
@@ -170,90 +98,15 @@ public:
         }
     };
 
-    // // Dictionary, Key is (current_location, target_location), Value is path to get there.
-    // std::unordered_map<double, double> PathingLookupDictionary;
-    // std::unordered_map<std::vector<double>, double,container_hash<std::vector<double>> > PathingLookupDictionary;
-    // std::unordered_map<std::vector<double>, std::vector<double>> PathingLookupDictionary;
+
     std::unordered_map<std::vector<std::string>, std::vector<PresetLocation>, container_hash<std::vector<std::string>> > PathingLookupDictionary;
-    // std::unordered_map<std::vector<std::string>, std::vector<PresetLocation>, container_hash<std::vector<std::string>> > PathingLookupDictionary = {
-    //     { {"start_a", "bin3_a"} , std::vector<PresetLocation>{start_a, bin3_a} },
-    //     { {"start_a", "agv2_a"} , std::vector<PresetLocation>{start_a, agv2_a} },
-    //     { {"start_a", "agv1_staging_a"} , std::vector<PresetLocation>{start_a, agv1_staging_a} },
-    //     { {"start_a", "bottom_left_staging_a"} , std::vector<PresetLocation>{start_a, agv1_staging_a, bottom_left_staging_a} },
-    //     { {"start_a", "shelf8_a"} , std::vector<PresetLocation>{start_a, mid_5_8_staging_a, shelf8_a} },
-    //     { {"start_a", "shelf11_a"} , std::vector<PresetLocation>{start_a, mid_8_11_staging_a, shelf11_a} },
-    //     { {"start_a", "bin11_a"} , std::vector<PresetLocation>{start_a, bin11_a} },
 
-    //     { {"bin3_a", "start_a"} , std::vector<PresetLocation>{bin3_a, start_a} },
-    //     { {"agv2_a", "start_a"} , std::vector<PresetLocation>{agv2_a, start_a} },
-    //     { {"agv1_staging_a", "start_a"} , std::vector<PresetLocation>{agv1_staging_a, start_a} },
-    //     { {"bottom_left_staging_a", "start_a"} , std::vector<PresetLocation>{bottom_left_staging_a, agv1_staging_a, start_a} },
-    //     { {"shelf8_a", "start_a"} , std::vector<PresetLocation>{shelf8_a, mid_5_8_staging_a, start_a} },
-    //     { {"shelf11_a", "start_a"} , std::vector<PresetLocation>{shelf11_a, mid_8_11_staging_a, start_a} },
-    //     { {"bin11_a", "start_a"} , std::vector<PresetLocation>{bin11_a, start_a} },
+    double calcDistanceInXYPlane(geometry_msgs::Pose a, geometry_msgs::Pose b);
+    PresetLocation getNearesetPresetLocation();
+    std::vector<PresetLocation> getPresetLocationVector(PresetLocation target_preset_location);
+    bool executeVectorOfPresetLocations( std::vector<PresetLocation> path_to_execute );
+    geometry_msgs::Pose gantryXY2worldposeXY(PresetLocation preset_location_2_convert);
 
-    // };
-
-    // std::unordered_map<std::vector<double>, std::vector<double>> PathingLookupDictionary = {
-    //     { {10.0, 11.3} , {11.2, 11.44, 11.556} }
-
-    // };
-
-
-
-    std::vector<PresetLocation> getPresetLocationVector(PresetLocation target_preset_location) {
-        ROS_INFO_STREAM("inside get Preset Location Vector function executed!");
-        PresetLocation approximate_current_position = getNearesetPresetLocation();
-        ROS_INFO_STREAM("getNearesetPresetLocation executed! =====");
-        std::vector<std::string> key = {{approximate_current_position.name, target_preset_location.name}};
-        ROS_INFO_STREAM("key executed! ");
-
-        ROS_INFO_STREAM("key executed! =====" << key[0] << " " << key[1]);
-
-        std::vector<PresetLocation> path_to_execute = PathingLookupDictionary.at(key);
-        ROS_INFO_STREAM("path_to_execute lookup executed! =====");
-        return path_to_execute;
-    }
-
-
-    bool executeVectorOfPresetLocations( std::vector<PresetLocation> path_to_execute ) {
-        for (PresetLocation psetlocation : path_to_execute) {
-            gantry_->goToPresetLocation(psetlocation);
-        }
-        return true;
-    }
-
-
-
-    // converts PresetLocation to a Pose, but only look at Pose's x and y, other numbers are meaningless
-    geometry_msgs::Pose gantryXY2worldposeXY(PresetLocation preset_location_2_convert) {
-        ROS_INFO_STREAM("gantryXY 2 worldpose XY entered");
-
-        ROS_INFO_STREAM(preset_location_2_convert.name);
-        ROS_INFO_STREAM(preset_location_2_convert.gantry.empty());
-
-
-        double gantry_x = preset_location_2_convert.gantry[0];
-        ROS_INFO_STREAM("double gantry_x defined successfully");
-
-        double gantry_y = preset_location_2_convert.gantry[1];
-        ROS_INFO_STREAM("double gantry_y defined successfully");
-
-        geometry_msgs::Pose converted_pose;
-        ROS_INFO_STREAM("converted_pose defined successfully");
-
-        converted_pose.position.x = gantry_x;
-        ROS_INFO_STREAM("set equals defined successfully");
-
-        converted_pose.position.y = gantry_y * -1;
-        ROS_INFO_STREAM("multiplication done successfully");
-
-        ROS_INFO_STREAM("gantryXY 2 worldpose XY ran sucessfully");
-
-
-        return converted_pose;
-
-    }
 
 };
 
--- rwa4_group_2/src/rwa_implementation.cpp
+++ rwa4_group_2/src/rwa_implementation.cpp
@@ -439,3 +439,127 @@ void RWAImplementation::checkAgvErrors() {
 // bool RWAImplementation::checkAndCorrectPose(){
 
 // }
+
+
+
+
+double RWAImplementation::calcDistanceInXYPlane(geometry_msgs::Pose a, geometry_msgs::Pose b) {
+    return std::sqrt( std::pow(a.position.x - b.position.x, 2) + std::pow(a.position.y - b.position.y, 2) ); // euclidean distance
+}
+
+
+
+
+
+//////////////////////////////// Get nearest preset location to current gantry position
+PresetLocation RWAImplementation::getNearesetPresetLocation() {
+    ////// Get current gantry position
+    geometry_msgs::Pose current_position = gantry_->getGantryPose();
+
+    ROS_INFO_STREAM("finished getGantry Pose successfully executed!");
+
+    ////// Define custom struct (distance, PresetLocation), see https://www.cplusplus.com/articles/NhA0RXSz/
+
+    ROS_INFO_STREAM("struct defined successfully");
+
+
+    ////// Build vector of custom structs
+    std::vector<distance_and_PresetLocation_struct> vector_of_structs;
+
+    ROS_INFO_STREAM("std::vector of structs ran");
+
+    ROS_INFO_STREAM("size of preset vector ==== " << preset_locations_list_.size());
+
+    ROS_INFO_STREAM("name of 1st of preset vector ==== " << preset_locations_list_.at(0).name);
+
+
+    for (PresetLocation pset_location : preset_locations_list_) {
+        ROS_INFO_STREAM("for loop entered");
+
+        geometry_msgs::Pose  location_converted_to_pose = gantryXY2worldposeXY(pset_location); // convert preset location to pose
+        ROS_INFO_STREAM("location_converted_to_pose in loop ran");
+
+        double distance_i = calcDistanceInXYPlane(current_position, location_converted_to_pose); // euclidean dist in xy plane
+        ROS_INFO_STREAM("calcDistanceInXYPlane in loop ran");
+
+        distance_and_PresetLocation_struct candidate_struct {distance_i, pset_location}; // make a struct
+        ROS_INFO_STREAM("candidate_struct in loop ran");
+
+        vector_of_structs.push_back(candidate_struct); // put struct in list of structs
+        ROS_INFO_STREAM("push_back in loop ran");
+
+    }
+
+    ROS_INFO_STREAM("vector of custom structs defined successfully");
+
+
+    ////// Sort vector of custom structs by distance (need *another* function to do this, will define a lambda function)
+    std::sort(vector_of_structs.begin(), vector_of_structs.end(),
+        // Lambda expression begins,
+        [](const distance_and_PresetLocation_struct &lhs, const distance_and_PresetLocation_struct &rhs) {
+            return (lhs.distance < rhs.distance);
+        } // end of lambda expression
+    );
+
+    ROS_INFO_STREAM("sorted vector successfully");
+
+
+    ////// Return the nearest PresetLocation
+    return vector_of_structs[0].candidate_location;
+}
+
+
+
+std::vector<PresetLocation> RWAImplementation::getPresetLocationVector(PresetLocation target_preset_location) {
+    ROS_INFO_STREAM("inside get Preset Location Vector function executed!");
+    PresetLocation approximate_current_position = getNearesetPresetLocation();
+    ROS_INFO_STREAM("getNearesetPresetLocation executed! =====");
+    std::vector<std::string> key = {{approximate_current_position.name, target_preset_location.name}};
+    ROS_INFO_STREAM("key executed! ");
+
+    ROS_INFO_STREAM("key executed! =====" << key[0] << " " << key[1]);
+
+    std::vector<PresetLocation> path_to_execute = PathingLookupDictionary.at(key);
+    ROS_INFO_STREAM("path_to_execute lookup executed! =====");
+    return path_to_execute;
+}
+
+
+bool RWAImplementation::executeVectorOfPresetLocations( std::vector<PresetLocation> path_to_execute ) {
+    for (PresetLocation psetlocation : path_to_execute) {
+        gantry_->goToPresetLocation(psetlocation);
+    }
+    return true;
+}
+
+
+
+// converts PresetLocation to a Pose, but only look at Pose's x and y, other numbers are meaningless
+geometry_msgs::Pose RWAImplementation::gantryXY2worldposeXY(PresetLocation preset_location_2_convert) {
+    ROS_INFO_STREAM("gantryXY 2 worldpose XY entered");
+
+    ROS_INFO_STREAM(preset_location_2_convert.name);
+    ROS_INFO_STREAM(preset_location_2_convert.gantry.empty());
+
+
+    double gantry_x = preset_location_2_convert.gantry[0];
+    ROS_INFO_STREAM("double gantry_x defined successfully");
+
+    double gantry_y = preset_location_2_convert.gantry[1];
+    ROS_INFO_STREAM("double gantry_y defined successfully");
+
+    geometry_msgs::Pose converted_pose;
+    ROS_INFO_STREAM("converted_pose defined successfully");
+
+    converted_pose.position.x = gantry_x;
+    ROS_INFO_STREAM("set equals defined successfully");
+
+    converted_pose.position.y = gantry_y * -1;
+    ROS_INFO_STREAM("multiplication done successfully");
+
+    ROS_INFO_STREAM("gantryXY 2 worldpose XY ran sucessfully");
+
+
+    return converted_pose;
+
+}
