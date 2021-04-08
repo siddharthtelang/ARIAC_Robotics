022ff9def84e638b6821bee2507f2d9419405e41
tangm7 <mack.tang@colorado.edu>
2021-04-08 17:19:13 -0600

    move drop part code from .h to .cpp, deleted many debugging print statements for presetLocationVector stuff

--- rwa4_group_2/src/rwa_implementation.cpp
+++ rwa4_group_2/src/rwa_implementation.cpp
@@ -464,42 +464,19 @@ PresetLocation RWAImplementation::getNearesetPresetLocation() {
     ////// Get current gantry position
     geometry_msgs::Pose current_position = gantry_->getGantryPose();
 
-    ROS_INFO_STREAM("finished getGantry Pose successfully executed!");
-
     ////// Define custom struct (distance, PresetLocation), see https://www.cplusplus.com/articles/NhA0RXSz/
-
-    ROS_INFO_STREAM("struct defined successfully");
-
 
     ////// Build vector of custom structs
     std::vector<distance_and_PresetLocation_struct> vector_of_structs;
 
-    ROS_INFO_STREAM("std::vector of structs ran");
-
-    ROS_INFO_STREAM("size of preset vector ==== " << preset_locations_list_.size());
-
-    ROS_INFO_STREAM("name of 1st of preset vector ==== " << preset_locations_list_.at(0).name);
-
 
     for (PresetLocation pset_location : preset_locations_list_) {
-        ROS_INFO_STREAM("for loop entered");
-
         geometry_msgs::Pose  location_converted_to_pose = gantryXY2worldposeXY(pset_location); // convert preset location to pose
-        ROS_INFO_STREAM("location_converted_to_pose in loop ran");
-
         double distance_i = calcDistanceInXYPlane(current_position, location_converted_to_pose); // euclidean dist in xy plane
-        ROS_INFO_STREAM("calcDistanceInXYPlane in loop ran");
-
         distance_and_PresetLocation_struct candidate_struct {distance_i, pset_location}; // make a struct
-        ROS_INFO_STREAM("candidate_struct in loop ran");
-
         vector_of_structs.push_back(candidate_struct); // put struct in list of structs
-        ROS_INFO_STREAM("push_back in loop ran");
 
     }
-
-    ROS_INFO_STREAM("vector of custom structs defined successfully");
-
 
     ////// Sort vector of custom structs by distance (need *another* function to do this, will define a lambda function)
     std::sort(vector_of_structs.begin(), vector_of_structs.end(),
@@ -519,16 +496,11 @@ PresetLocation RWAImplementation::getNearesetPresetLocation() {
 
 
 std::vector<PresetLocation> RWAImplementation::getPresetLocationVector(PresetLocation target_preset_location) {
-    ROS_INFO_STREAM("inside get Preset Location Vector function executed!");
     PresetLocation approximate_current_position = getNearesetPresetLocation();
-    ROS_INFO_STREAM("getNearesetPresetLocation executed! =====");
     std::vector<std::string> key = {{approximate_current_position.name, target_preset_location.name}};
-    ROS_INFO_STREAM("key executed! ");
-
     ROS_INFO_STREAM("key executed! =====" << key[0] << " " << key[1]);
-
     std::vector<PresetLocation> path_to_execute = PathingLookupDictionary.at(key);
-    ROS_INFO_STREAM("path_to_execute lookup executed! =====");
+    ROS_INFO_STREAM("path_to_execute lookup executed!");
     return path_to_execute;
 }
 
@@ -544,28 +516,12 @@ bool RWAImplementation::executeVectorOfPresetLocations( std::vector<PresetLocati
 
 // converts PresetLocation to a Pose, but only look at Pose's x and y, other numbers are meaningless
 geometry_msgs::Pose RWAImplementation::gantryXY2worldposeXY(PresetLocation preset_location_2_convert) {
-    ROS_INFO_STREAM("gantryXY 2 worldpose XY entered");
-
-    ROS_INFO_STREAM(preset_location_2_convert.name);
-    ROS_INFO_STREAM(preset_location_2_convert.gantry.empty());
-
 
     double gantry_x = preset_location_2_convert.gantry[0];
-    ROS_INFO_STREAM("double gantry_x defined successfully");
-
     double gantry_y = preset_location_2_convert.gantry[1];
-    ROS_INFO_STREAM("double gantry_y defined successfully");
-
     geometry_msgs::Pose converted_pose;
-    ROS_INFO_STREAM("converted_pose defined successfully");
-
     converted_pose.position.x = gantry_x;
-    ROS_INFO_STREAM("set equals defined successfully");
-
     converted_pose.position.y = gantry_y * -1;
-    ROS_INFO_STREAM("multiplication done successfully");
-
-    ROS_INFO_STREAM("gantryXY 2 worldpose XY ran sucessfully");
 
 
     return converted_pose;
