1cb17404c17031dde5d93bc173e8f159a58aca93
amanvirmani <aman.virmani26@gmail.com>
2021-04-18 03:19:39 -0400

    updated parts on conveyor list to check if cam_parts is empty

--- rwa4_group_2/src/camera_listener.cpp
+++ rwa4_group_2/src/camera_listener.cpp
@@ -274,7 +274,8 @@ void CameraListener::breakbeam_callback(const nist_gear::Proximity::ConstPtr &ms
     ros::Time t_hit_breakbeam = ros::Time::now();
     load_time_on_conveyor_.push(t_hit_breakbeam);
     auto cam_parts = fetchPartsFromCamera(node_, 5);
-    parts_on_conveyor_.push_back(cam_parts[0]);
+    if (!cam_parts.empty())
+        parts_on_conveyor_.push_back(cam_parts[0]);
 
     // ROS_INFO_STREAM("\n=========================================\n"
     // << "Objected detected from breakbeam! Time: " << t_hit_breakbeam << "\n=========================================\n");
