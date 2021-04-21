245623361610ecd38c3bb6aef4b5b5008137b7d3
amanvirmani <aman.virmani26@gmail.com>
2021-04-21 19:50:12 -0400

    deleted redundant build files

--- rwa5_group_2/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/dani/ariac_ws/src/ARIAC_Robotics/rwa4_group_2/include;/usr/include/eigen3".split(';') if "/home/dani/ariac_ws/src/ARIAC_Robotics/rwa4_group_2/include;/usr/include/eigen3" != "" else []
-PROJECT_CATKIN_DEPENDS = "moveit_core;moveit_visual_tools;moveit_ros_planning_interface;interactive_markers;tf2_geometry_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "rwa4_group_2"
-PROJECT_SPACE_DIR = "/home/dani/ariac_ws/src/ARIAC_Robotics/rwa4_group_2/build/devel"
-PROJECT_VERSION = "0.0.0"
