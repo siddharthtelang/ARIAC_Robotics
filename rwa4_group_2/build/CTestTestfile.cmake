5b2292d448599dd727bc014d742d3d1b920e6ede
Siddharth Telang <stelang@umd.edu>
2021-06-10 15:25:49 -0400

    Remove build folder

--- rwa4_group_2/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dani/ariac_ws/src/ARIAC_Robotics/rwa4_group_2
-# Build directory: /home/dani/ariac_ws/src/ARIAC_Robotics/rwa4_group_2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
